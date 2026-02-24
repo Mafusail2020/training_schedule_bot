d4ae601989dc43ef9d7b8b501e1bb7ba83f3ba92
Mafusail2020 <krutkev00@gmail.com>
2026-02-24 19:38:00 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from app.bot.bot import bot, dp
 import asyncio
 import logging
-
-logging.basicConfig(level=logging.INFO)
+from app.database.db import db
 
 async def main():
+    await db.connect()
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("[#] PROG EXIT")
